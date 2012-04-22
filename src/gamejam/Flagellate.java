68ea67ba7b03e30feb6b657ef766041abe5e5557
admin <admin@76.78.253.24>
2012-04-21 22:22:16 -0400

    Flagellate intruder has been implemented.

---
+++ src/gamejam/Flagellate.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gamejam;
+import java.awt.color.*;
+
+/**
+ *
+ * @author Clem
+ */
+public class Flagellate extends Intruder{
+    
+    boolean hascrashed;
+    
+    public Flagellate()
+    {
+        bounces = true;
+      x = Math.random() * Engine.getWidth();
+      y = Math.random() * Engine.getHeight();
+      maxVel = 5.9;
+      vel = Math.random() * maxVel;
+      theta = fTheta = Math.random() * Math.PI * 2;
+      sprite = null;
+      ratUp = 6;
+      ratDown = 8;
+      primeDist = sprite.getSpriteWidth()/2;
+      maxHp = hp = 500;
+      hascrashed=true;
+    }
+    
+    public void act()
+    {
+        if (hascrashed)
+            target=Engine.getBloodVessel().nearestTower(this);
+    }
+    
+    //The flagellate slams into a target and knocks it back, then finds a new target.
+    //the Flagellate needs to be more massive than you'd think it would be based on its size.
+    public void onCollision(Entity other)
+    {
+        if (other instanceof Tower)
+        {
+            other.damage(35);
+            hascrashed=true;
+        }
+        else
+            super.onCollision(other);
+    }
+
+}
